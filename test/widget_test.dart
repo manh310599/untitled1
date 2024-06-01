23d3d30a2b4094c3cf529652d00e2a6570de394f
manh310599 <manhmanh310599@gmail.com>
2024-06-01 03:09:59 -0700

    three commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,6 @@ import 'package:untitled1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-
     main();
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
